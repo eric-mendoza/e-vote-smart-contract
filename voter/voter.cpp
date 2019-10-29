50f11b7097ba9392d73ded7a64ed1337f4ed7088
eric-mendoza <eric.mendoza333@gmail.com>
2019-10-29 00:21:16 -0600

    Voters indexed by hashed dpi smart contract.

--- voter/voter.cpp
+++ voter/voter.cpp
@@ -1,1 +1,16 @@
 #include "voter.h"
+
+[[eosio::action]]
+void voter::addvoter(name election_name, uint64_t) {
+    print("Added.");
+};
+
+[[eosio::action]]
+void voter::erasevoter(name election_name, uint64_t dpi) {
+    print("Erased");
+};
+
+[[eosio::action]]
+void voter::vote(name election_name, uint64_t dpi, string presidente, string alcalde) {
+    print("voted");
+};
--- voter/voter.h
+++ voter/voter.h
@@ -12,18 +12,32 @@ class [[eosio::contract("voter")]] voter : public eosio::contract {
 private:
     // Create a table for voters.
     struct [[eosio::table]] voters {
-        uint64_t        dpi;
-        bool            voted;
+        uint64_t        id;
+        checksum256     dpi;  // cifrado
+        uint64_t        mesa_id;  // This is only saved in API db
+        bool            voted = false;
+        bool            enabled = false;  // This is enabled before voting
 
-        uint64_t primary_key() const { return dpi; }
+        // Primary key
+        uint64_t primary_key() const { return id; }
+
+        // Secondary key
+        checksum256 by_dpi() const { return get_dpi(dpi); }
+
+        // Convert sha256 to uint64_t
+        static checksum256 get_dpi(const checksum256 &dpi) {
+            const uint64_t *p64 = reinterpret_cast<const uint64_t *>(&dpi);
+            return checksum256::make_from_word_sequence<uint64_t>(p64[0], p64[1], p64[2], p64[3]);
+        }
     };
 
-    typedef eosio::multi_index<name("voters"), election> voters_table;
-
+    typedef eosio::multi_index<name("voters"), voters,
+            indexed_by<name("bydpi"), const_mem_fun<voters, checksum256, &voters::by_dpi>>
+            > voters_table;
 
 public:
     // Constructor
-    voter( name receiver, name code, datastream<const char*> ds ):contract(receiver, code, ds) {}
+    voter(name receiver, name code, datastream<const char*> ds ):contract(receiver, code, ds) {}
 
     [[eosio::action]]
     void addvoter(name election_name, uint64_t);
@@ -33,9 +47,5 @@ public:
 
     [[eosio::action]]
     void vote(name election_name, uint64_t dpi, string presidente, string alcalde);
-
-
 };
-
-
 #endif //EVOTE_VOTER_H
